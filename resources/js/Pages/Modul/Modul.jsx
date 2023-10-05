b16c329ea088458d970f3e4e6b3a2a9b7e3b52e6
GerinAryoPrasetia <gerinaryo14@gmail.com>
2023-10-05 18:58:44 +0700

    add more field on working permits

--- resources/js/Pages/Modul/Modul.jsx
+++ resources/js/Pages/Modul/Modul.jsx
@@ -8,15 +8,15 @@ export default function Modul({ auth }) {
             <div className="container w-full m-auto">
                 <h1 className="text-center font-bold mt-4">Pilih Modul</h1>
                 <Link href={route("working-permits.index")}>
-                    <div className="m-auto p-4 bg-white my-4 rounded-lg mx-6">
-                        <p className="text-center">Working Permits</p>
+                    <div className="m-auto p-4 bg-gray-100 my-4 rounded-lg mx-6">
+                        <p className="text-center font-bold">Working Permits</p>
                     </div>
                 </Link>
-                <div className="m-auto p-4 bg-white my-4 rounded-lg mx-6">
-                    <p className="text-center">APAR</p>
+                <div className="m-auto p-4 bg-gray-100 my-4 rounded-lg mx-6">
+                    <p className="text-center font-bold">APAR</p>
                 </div>
-                <div className="m-auto p-4 bg-white my-4 rounded-lg mx-6">
-                    <p className="text-center">Incidents</p>
+                <div className="m-auto p-4 bg-gray-100 my-4 rounded-lg mx-6">
+                    <p className="text-center font-bold">Incidents</p>
                 </div>
             </div>
         </AuthenticatedLayout>
--- resources/js/Pages/Modul/WorkingPermits/Index.jsx
+++ resources/js/Pages/Modul/WorkingPermits/Index.jsx
@@ -1,7 +1,7 @@
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import React from "react";
+import React, { useState } from "react";
 import { Collapse } from "antd";
 import { useForm } from "@inertiajs/react";
 import TextInput from "@/Components/TextInput";
@@ -9,14 +9,104 @@ import InputError from "@/Components/InputError";
 import TextAreaInput from "@/Components/TextAreaInput";
 import Dropdown from "@/Components/Dropdown";
 import Select from "@/Components/Select";
+import Checkbox from "@/Components/Checkbox";
 
 export default function WorkingPermits({ auth }) {
+    const checkboxData = [
+        //work area
+        { id: "Hazardous area", label: "Hazardous area" },
+        { id: "Confined space", label: "Confined space" },
+        { id: "Contaminated atmosphere", label: "Contaminated atmosphere" },
+        { id: "Explosive atmosphere", label: "Explosive atmosphere" },
+        {
+            id: "Hostile environment/weather",
+            label: "Hostile environment/weather",
+        },
+        { id: "Near/over water", label: "Near/over water" },
+        { id: "Isolated area", label: "Isolated area" },
+        { id: "Elevated area", label: "Elevated area" },
+        { id: "Other area", label: "Other area" },
+    ];
+
+    const checkboxDataNatureofWork = [
+        //nature of work
+        { id: "Construction", label: "Construction" },
+        { id: "Erection", label: "Erection" },
+        { id: "Using hazardous chemical", label: "Using hazardous chemical" },
+        { id: "Modification", label: "Modification" },
+        { id: "Hot work", label: "Hot work" },
+        { id: "Electrical work", label: "Electrical work" },
+        { id: "Repair", label: "Repair" },
+        { id: "Removal", label: "Removal" },
+        { id: "Personnel lifting", label: "Personnel lifting" },
+        { id: "Maintenance", label: "Maintenance" },
+        { id: "Deviation from Procedure", label: "Deviation from Procedure" },
+        { id: "Pressurized operation", label: "Pressurized operation" },
+        {
+            id: "Working on safety equipments",
+            label: "Working on safety equipments",
+        },
+        { id: "Demolition", label: "Demolition" },
+        { id: "Using explosives", label: "Using explosives" },
+        { id: "Excavation", label: "Excavation" },
+        { id: "Other activity", label: "Other activity" },
+    ];
+    const [checkedItemsWorkArea, setCheckedItemsWorkArea] = useState([]);
+    const [checkedItemsNatureofWork, setCheckedItemsNatureofWork] = useState(
+        []
+    );
+    const handleCheckboxChange = (event) => {
+        const { id, checked } = event.target;
+
+        // Clone the current state array to avoid mutating it directly
+        const updatedCheckedItems = [...checkedItemsWorkArea];
+
+        if (checked) {
+            // If the checkbox is checked, add the ID to the array
+            updatedCheckedItems.push(id);
+        } else {
+            // If the checkbox is unchecked, remove the ID from the array
+            const index = updatedCheckedItems.indexOf(id);
+            if (index !== -1) {
+                updatedCheckedItems.splice(index, 1);
+            }
+        }
+
+        // Update the state with the new array
+        setCheckedItemsWorkArea(updatedCheckedItems);
+        setData("workArea", updatedCheckedItems);
+    };
+
+    const handleCheckboxChangeNatureofWork = (event) => {
+        const { id, checked } = event.target;
+
+        // Clone the current state array to avoid mutating it directly
+        const updatedCheckedItems = [...checkedItemsNatureofWork];
+
+        if (checked) {
+            // If the checkbox is checked, add the ID to the array
+            updatedCheckedItems.push(id);
+        } else {
+            // If the checkbox is unchecked, remove the ID from the array
+            const index = updatedCheckedItems.indexOf(id);
+            if (index !== -1) {
+                updatedCheckedItems.splice(index, 1);
+            }
+        }
+
+        // Update the state with the new array
+        setCheckedItemsNatureofWork(updatedCheckedItems);
+        setData("natureOfWork", updatedCheckedItems);
+    };
+
     const { data, setData, post, processing, errors, reset } = useForm({
         email: "",
         password: "",
-        remember: false,
+        workArea: [],
+        natureOfWork: [],
     });
     const text = ``;
+
     const items = [
         {
             key: "1",
@@ -115,6 +205,59 @@ export default function WorkingPermits({ auth }) {
                     <div>
                         <InputLabel htmlFor="email" value="Permit to Work" />
                         <Select />
+                    </div>
+                    <div>
+                        <InputLabel htmlFor="email" value="Work Site" />
+                        <Select />
+                    </div>
+                    <div className="mt-4">
+                        <InputLabel htmlFor="email" value="Work Area" />
+                        {checkboxData.map((item) => (
+                            <div className="block mt-4">
+                                <label className="flex items-center">
+                                    <Checkbox
+                                        name="remember"
+                                        key={item.id}
+                                        id={item.id}
+                                        // // checked={data.remember}
+
+                                        checked={checkedItemsWorkArea.includes(
+                                            item.id
+                                        )}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className="ml-2 text-sm text-gray-600">
+                                        {item.label}
+                                    </span>
+                                </label>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="mt-4">
+                        <InputLabel
+                            htmlFor="email"
+                            value="Nature of work to be performed"
+                        />
+                        {checkboxDataNatureofWork.map((item) => (
+                            <div className="block mt-4">
+                                <label className="flex items-center">
+                                    <Checkbox
+                                        name="remember"
+                                        key={item.id}
+                                        id={item.id}
+                                        checked={checkedItemsNatureofWork.includes(
+                                            item.id
+                                        )}
+                                        onChange={
+                                            handleCheckboxChangeNatureofWork
+                                        }
+                                    />
+                                    <span className="ml-2 text-sm text-gray-600">
+                                        {item.label}
+                                    </span>
+                                </label>
+                            </div>
+                        ))}
                     </div>
                 </div>
             ),
