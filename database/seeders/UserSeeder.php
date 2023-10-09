311718ac1eb17bb6ccbf38ff82e92c39b8c1e0c0
GerinAryoPrasetia <gerin.aryo@efishery.com>
2023-10-09 23:39:45 +0700

    add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,25 +17,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        $createMultipleUsers = [
+            ['id' => Str::uuid(), 'name' => 'Admin Gerin Aryo', 'email' => 'admin@mail.com', 'password' => bcrypt('testpassword'), 'role' => 'admin', 'departement' => 'Security', 'position' => 'Manager II', 'is_vendor' => false, 'reference_id' => 'REF-ID-USER-1'],
+            ['id' => Str::uuid(), 'name' => 'Guest Gerin Aryo', 'email' => 'guest@mail.com', 'password' => bcrypt('testpassword'), 'role' => 'user', 'departement' => 'Security', 'position' => 'Manager II', 'is_vendor' => false, 'reference_id' => 'REF-ID-USER-2'],
+            ['id' => Str::uuid(), 'name' => 'Supervisor Gerin Aryo', 'email' => 'supervisor@mail.com', 'password' => bcrypt('testpassword'), 'role' => 'user', 'departement' => 'Security', 'position' => 'Supervisor', 'is_vendor' => false, 'reference_id' => 'REF-ID-USER-3'],
+            ['id' => Str::uuid(), 'name' => 'Vendor Gerin Aryo', 'email' => 'vendor@mail.com', 'password' => bcrypt('testpassword'), 'role' => 'user', 'departement' => 'Vendor', 'position' => 'Manager', 'is_vendor' => true, 'reference_id' => 'REF-ID-USER-4']
+        ];
+
+        User::insert($createMultipleUsers);
         DB::table('users')->insert(
-            // [
-            //     'id' => Str::uuid(),
-            //     'name' => 'test admin user',
-            //     'email' => 'admin@gmail.com',
-            //     'password' => Hash::make('testpassword'),
-            //     'role' => 'admin',
-            //     'departement' => 'Security',
-            //     'position' => 'Security',
-            // ],
-            [
-                'id' => Str::uuid(),
-                'name' => 'test user',
-                'email' => 'user@gmail.com',
-                'password' => Hash::make('testpassword'),
-                'role' => 'user',
-                'departement' => 'Security',
-                'position' => 'Manager II',
-            ]
+            $createMultipleUsers
         );
     }
 }
